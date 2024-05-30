2c526f496f2167171a41cb1fa8cb12058e0d5449
Алексей Оспельников <ospelnikov00@mail.ru>
2024-05-30 16:38:53 +0300

    feat: Added moving cars
    TODO: fix bad animations moving cars

--- Car_Estimator/scripts/main_page.js
+++ Car_Estimator/scripts/main_page.js
@@ -1,13 +1,38 @@
 window.onload = function(){
-    $("#white").css({"margin-right": "2500px"});
-    $("#white").css({"margin-bottom": "1000px"});
-    $(window).scroll(function() {
-        let scrollTop = $(window).scrollTop();
-        console.log("scrollTop>>>" + scrollTop);
-        $("#white").css(
-            {"margin-left": ($(window).scrollTop()) * 4 + "px",
-                   "margin-top": ($(window).scrollTop()) * 0.75 + "px"
-                    });
+    let whiteCar = $("#white"),
+        blackLeftCar = $("#black_left"),
+        blackRightCar = $("#black_right"),
+        screen = $(window);
+
+    const screenWidth = window.innerWidth,
+        animationTime = 5,
+        horizontalScale = 6,
+        verticalScale = 4,
+        whiteCarWidth = whiteCar.width(),
+        whiteCarHeight = whiteCar.height(),
+        blackCarWidth = blackLeftCar.width(),
+        blackCarHeight = blackLeftCar.height();
+
+    screen.scroll(function() {
+        let scrollTop = screen.scrollTop(),
+            horizontalMove = scrollTop * horizontalScale,
+            verticalMove = scrollTop * verticalScale;
+
+        whiteCar.animate({
+            "left": horizontalMove - whiteCarWidth + "px",
+            "top": verticalMove - whiteCarHeight + "px",
+           }, animationTime);
+
+        blackLeftCar.animate({
+            "left": horizontalMove - blackCarWidth + "px",
+            "top": verticalMove + "px",
+            }, animationTime);
+
+        blackRightCar.animate({
+            "left": horizontalMove + "px",
+            "top": verticalMove - blackCarHeight + "px",
+            }, animationTime);
+
     });
 
 };
